d3bb432929513f6170c2742c774304ddd2c69af1
techspecialist1 <techspecialist840@gmail.com>
2025-04-07 17:14:00 +0530

    Added required dependencies

---
+++ App.js
@@ -0,0 +1,14 @@
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const App = () => {
+  return (
+    <SafeAreaView style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>App</Text>
+    </SafeAreaView>
+  )
+}
+
+export default App
+
+const styles = StyleSheet.create({})
